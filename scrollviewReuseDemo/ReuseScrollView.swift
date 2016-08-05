50cafa9d889c11e321b673922801b6ffbc3027f4
李林凯 <lilinkai1990@gmail.com>
2016-08-05 16:39:18 +0800

    更新为加载网络图片

--- scrollviewReuseDemo/ReuseScrollView.swift
+++ scrollviewReuseDemo/ReuseScrollView.swift
@@ -12,7 +12,8 @@ class ReuseScrollView: UIView, UIScrollViewDelegate {
     
     private var contentScrollView:UIScrollView! = nil //滚动容器
     
-    private var imagesArray:[AnyObject]! = nil
+    private var imagesUrlArray:[String]! = nil
+    private var imagesArray:[UIImage]! = nil
     private var descsArray:[String]! = nil
     
     private var currentImageView:UIImageView! = nil //当前显示imageview
@@ -35,29 +36,6 @@ class ReuseScrollView: UIView, UIScrollViewDelegate {
     struct styleConfig {
         
         
-    }
-    
-    internal func initDataSource(imagesValue:[AnyObject], descsValue:[String]){
-        imagesArray = imagesValue
-        descsArray = descsValue
-        
-        if imagesArray.count != descsArray.count {
-            print("图片数量与描述数量不匹配!")
-            return
-        }
-        
-        contentScrollView.contentSize = CGSize.init(width: width*CGFloat(imagesArray.count), height: height)
-        contentScrollView.contentOffset = CGPoint.init(x: width, y: 0)
-        
-        currentImageView.setLeft(width)
-        nextImageView.setLeft(0)
-        
-        currentImageView.image = imagesArray[currentIndex] as? UIImage
-        nextImageView.image = imagesArray[nextIndex] as? UIImage
-        
-        descContentLabel.text = descsArray[currentIndex]
-        
-        pageControl.numberOfPages = imagesArray.count
     }
     
     /**
@@ -129,11 +107,76 @@ class ReuseScrollView: UIView, UIScrollViewDelegate {
         queue = NSOperationQueue()
     }
     
+    internal func initDataSource(imagesValue:[String], descsValue:[String]){
+        
+        imagesUrlArray = imagesValue
+        
+        descsArray = descsValue
+        
+        if imagesUrlArray.count != descsArray.count {
+            print("图片数量与描述数量不匹配!")
+            return
+        }
+        
+        addPlaceholderImage()
+        
+        downImageData()
+        
+        contentScrollView.contentSize = CGSize.init(width: width*CGFloat(imagesUrlArray.count), height: height)
+        contentScrollView.contentOffset = CGPoint.init(x: width, y: 0)
+        
+        currentImageView.setLeft(width)
+        nextImageView.setLeft(0)
+
+        descContentLabel.text = descsArray[currentIndex]
+        
+        pageControl.numberOfPages = imagesUrlArray.count
+    }
+    
+    /**
+     添加占位图
+     */
+    private func addPlaceholderImage(){
+        
+        imagesArray = [UIImage]()
+        for _ in 0..<imagesUrlArray.count {
+            imagesArray.append(UIImage.init(named: "1111")!)
+        }
+        
+        currentImageView.image = imagesArray[0]
+        nextImageView.image = imagesArray[0]
+    }
+    
     /**
      下载图片方法
      */
     private func downImageData(){
         
+        let operation = NSBlockOperation()
+        
+        for index in 0..<imagesUrlArray.count {
+            operation.addExecutionBlock({ [weak self] in
+                
+                let imageData = NSData.init(contentsOfURL: NSURL.init(string: self!.imagesUrlArray[index])!)
+                
+                if imageData == nil{
+                    print("图片数据为空")
+                    return
+                }
+                
+                let image = UIImage.init(data: imageData!)
+                
+                self?.imagesArray[index] = image!
+                
+                dispatch_async(dispatch_get_main_queue(), { 
+                    if self!.currentIndex == index{
+                        self?.currentImageView.image = image!
+                    }
+                })
+            })
+        }
+        
+        queue.addOperation(operation)
     }
     
     func scrollViewDidScroll(scrollView: UIScrollView){
@@ -150,19 +193,20 @@ class ReuseScrollView: UIView, UIScrollViewDelegate {
             //确定下一个索引
             nextIndex = currentIndex - 1
             if nextIndex < 0 {
-                nextIndex = imagesArray.count - 1
+                nextIndex = imagesUrlArray.count - 1
             }
-            nextImageView.image = imagesArray[nextIndex] as? UIImage
+            nextImageView.image = imagesArray[nextIndex]
             
             if offSet <= 0 {
                 changeNextImageView()
             }
         }
+            
         //向左滚动了
         else if offSet > width{
             nextImageView.setLeft(width*2)
-            nextIndex = (currentIndex + 1)%imagesArray.count
-            nextImageView.image = imagesArray[nextIndex] as? UIImage
+            nextIndex = (currentIndex + 1)%imagesUrlArray.count
+            nextImageView.image = imagesArray[nextIndex]
             
             if offSet >= width*2 {
                 changeNextImageView()
